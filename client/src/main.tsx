222aac2b1c32f5d51ce15f55874c6dec4f2e237e
Renchie <ryan0042@student.monash.edu>
2025-02-20 22:00:58 +1100

    updated export method for colors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./global.css";
 import App from "./App.tsx";
 import { WebSocketProvider } from "./contexts/WSContext.tsx";
-import { colors } from "../../shared/colors.ts";
+import "../../shared/colors.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- shared/colors.ts
+++ shared/colors.ts
@@ -1,7 +1,9 @@
 // pre-load for tailwind class inclusion
 
-export const colors = {
+const colors = {
   mafia: "text-red-600",
   towns: "text-green-600",
   neutral: "text-gray-600",
 };
+
+export default { colors };
